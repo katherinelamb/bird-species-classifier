cbe6c1c0cdd6be64711a4d60a6c5790add6d6007
Katie Lamb <katie@upstream.tech>
2020-05-16 13:25:52 -0600

    basic cnn, resnet finetuned with fully connected

---
+++ cnn.py
@@ -0,0 +1,204 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as T
+import torchvision.datasets as dset
+from torchvision import models
+
+
+# Use GPU if applicable, taken from Assignment 2 notebook
+USE_GPU = True
+
+dtype = torch.float32 
+
+if USE_GPU and torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+print('using device:', device)
+
+
+# Set up a transform to preprocess the data by subtracting the mean RGB value and dividing by the 
+# standard deviation of each RGB value
+transform = T.Compose(
+    [T.ToTensor(),
+    T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+
+# Load data
+trainset = dset.ImageFolder(root="../100-bird-species/train/", transform=transform)
+trainloader = DataLoader(trainset, batch_size=64, num_workers=0, shuffle=True)
+
+valset = torchvision.datasets.ImageFolder(root="../100-bird-species/valid/", transform=transform)
+valloader = DataLoader(valset, batch_size=64, num_workers=0, shuffle=False)
+
+testset = torchvision.datasets.ImageFolder(root="../100-bird-species/test/", transform=transform)
+testloader = DataLoader(testset, batch_size=64, num_workers=0, shuffle=False)
+
+dataloaders = {
+    "train": trainloader,
+    "val": valloader,
+    "test": testloader
+}
+datasizes = {
+    "train": len(trainset),
+    "val": len(valset),
+    "test": len(testset)
+}
+CLASSES = list(trainset.class_to_idx.keys())
+
+# View a minibatch of data - from Vipin Chaudhary's Kaggle notebook
+def imshow(img, size=(10, 10)):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    if size:
+        plt.figure(figsize=size)
+    
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.title("One mini batch")
+    plt.axis("off")
+    plt.pause(8)
+
+
+#dataiter = iter(trainloader)
+#images, labels = dataiter.next()
+#imshow(torchvision.utils.make_grid(images))
+
+# Adapted from Pytorch documentation and Assignment 2
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    model = model.to(device=device)
+
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device=device, dtype=dtype)
+                labels = labels.to(device=device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / datasizes[phase]
+            epoch_acc = running_corrects.double() / datasizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+def imshowaxis(ax, img, orig, pred):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    ax.imshow(np.transpose(npimg, (1, 2, 0)))
+    if orig != pred:
+        ax.set_title(orig + "\n" + pred, color="red")
+    else:
+        ax.set_title(orig + "\n" + pred)
+    ax.axis("off")
+
+def vis_model(model, num_images=25):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    figure, ax = plt.subplots(5, 5, figsize=(20, 20))
+    
+    
+    with torch.no_grad():
+        for i , (inputs, labels) in enumerate(dataloaders["test"]):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            
+            for i in range(5):
+                for j in range(5):
+                    if images_so_far < num_images:
+                        imshowaxis(ax[i][j], inputs.cpu().data[images_so_far], CLASSES[labels[images_so_far]], CLASSES[preds[images_so_far]])
+                    else:
+                        model.train(mode=was_training)
+                        return
+                    images_so_far += 1
+        model.train(mode=was_training)
+
+
+# Adapted from Pytorch transfer learning documentation
+# Fine tune pretrained resnet, add fully connected layer to output class scores
+model_conv = torchvision.models.resnet18(pretrained=True)
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, len(CLASSES))
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+model_conv = train_model(model_conv, criterion, optimizer_conv,
+                         exp_lr_scheduler, num_epochs=25)
+
+# visualize predictions
+vis_model(model_conv)
